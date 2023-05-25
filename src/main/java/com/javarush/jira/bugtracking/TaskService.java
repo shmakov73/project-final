c94f8b16bf3792bcdeadb120d667974c6cfa4efd
shmakov73 <shmakov73@gmail.com>
2023-05-26 00:14:35 +0400

    Refactored methods for adding tags to a task and subscribing a user to tasks, as well as adding tests to them.

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -12,14 +12,16 @@ import com.javarush.jira.login.User;
 import com.javarush.jira.login.internal.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
 
+    private static final int MAX_TAG_LENGTH = 32;
+    private static final int MIN_TAG_LENGTH = 2;
     private final UserRepository userRepository;
 
     private final UserBelongRepository userBelongRepository;
@@ -42,22 +44,26 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
     }
 
     private Set<String> validateTags(Set<String> tags) {
-        tags.removeIf(tag -> tag.length() < 2 || tag.length() > 32);
-        return tags;
+        if (tags == null) {
+            throw new IllegalArgumentException("The tags argument must not be null");
+        }
+        Set<String> validatedTags = tags.stream()
+                .filter(tag -> tag.length() >= MIN_TAG_LENGTH && tag.length() <= MAX_TAG_LENGTH)
+                .collect(Collectors.toUnmodifiableSet());
+        if (validatedTags.size() != tags.size()) {
+            throw new IllegalArgumentException("The tag length is out of bound");
+        }
+        return validatedTags;
     }
 
     public void subscribeUserToTask(Long taskId, Long userId) {
         User user = userRepository.getExisted(userId);
-        Set<Role> roles = user.getRoles();
-        if (roles.size() > 0){
-            Iterator<Role> iterator = roles.iterator();
-            Role role = iterator.next();
-                UserBelong userBelong = new UserBelong();
-                userBelong.setUserId(user.getId());
-                userBelong.setObjectId(taskId);
-                userBelong.setObjectType(ObjectType.TASK);
-                userBelong.setUserTypeCode(role.toString());
-                userBelongRepository.save(userBelong);
-        }
+        Role role = user.getRoles().stream().findFirst().orElseThrow(() -> new RuntimeException("User has no roles"));
+        UserBelong userBelong = new UserBelong();
+        userBelong.setUserId(user.getId());
+        userBelong.setObjectId(taskId);
+        userBelong.setObjectType(ObjectType.TASK);
+        userBelong.setUserTypeCode(role.toString());
+        userBelongRepository.save(userBelong);
     }
 }
